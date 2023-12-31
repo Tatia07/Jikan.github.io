3da4ef8a194a75f5cc3bb9a74de0e9c3d58a3abe
Tatia <142615214+Tatia07@users.noreply.github.com>
2023-12-31 21:37:48 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+function search() {
+    var query = document.getElementById('search_query').value;
+
+    var requestUrl = `https://api.jikan.moe/v4/anime?q=${query}&sfw`;
+    document.getElementById('search_query_url').innerHTML = 'fetching...';
+    document.getElementById('search_query_url').href = 'javascript:void(null)';
+
+    let startTime = new Date().getTime();
+
+    fetch(requestUrl)
+    .then(response => response.json())
+    .then(data => {
+        let timeTaken = new Date().getTime() - startTime;
+
+        document.getElementById('search_query_url').innerHTML = requestUrl;
+        document.getElementById('search_query_url').href = requestUrl;
+
+
+        var node = document.getElementById('search_results');
+        while (node.firstChild) {node.removeChild(node.firstChild);}
+
+        const maxResults = 16;
+        let i = 1;
+        
+        try {
+            data.data.forEach(item => {
+                if (i > maxResults) {
+                    throw BreakException;
+                }
+    
+                document.getElementById('search_results')
+                .insertAdjacentHTML(
+                    'beforeend',
+                    `
+                    <a href="${item.url}" class="card">
+                        <div class="card__image">
+                            <img loading="lazy" src="${item.images.jpg.large_image_url}" alt="${item.title}" />
+                        </div>
+                        <div class="card__name">
+                            <span>${item.title}</span> 
+                        </div>
+                    </a>
+                    `
+                );
+    
+                i++;
+            });
+        } catch (e) {
+        }
+
+    });
+}
+
+document.getElementById('search').onclick = () => search();
+document.getElementById('search_query').onkeydown = (event) => {
+    if (event.keyCode === 13) {
+        search();
+    }
+};
